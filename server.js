5e007d06294a88445e718189fb69f32192cbe4f8
sabeeh6 <sabeehrajput6@gmail.com>
2026-04-10 15:50:48 +0500

    vercel set up

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ import { routes } from "./routes/index.js"
 dotenv.config()
 const app = express()
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 Mongo_Db_Connection()
 app.use('/api' , routes)
 
-app.listen(process.env.PORT , ()=>{
-  logger.info("Server is running on Port 🛸");
-})
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5002;
+  app.listen(PORT, () => {
+    logger.info(`Server is running on Port ${PORT} 🛸`);
+  });
+}
+
+export default app;
